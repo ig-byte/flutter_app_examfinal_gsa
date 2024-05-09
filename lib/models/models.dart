f89e68e82bce59198db11d8bdf13e6b900259c52
ig.byte <gabriel.silva.aravena@gmail.com>
2024-05-09 18:41:52 -0400

    fix

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,7 +1,3 @@
 export 'package:flutter_app_examfinal_gsa/models/category_model.dart';
-<<<<<<< HEAD
-import 'package:flutter_app_examfinal_gsa/models/product_model.dart';
-=======
 export 'package:flutter_app_examfinal_gsa/models/product_model.dart';
->>>>>>> 412a86c (actualizacion de librerias)
 export 'package:flutter_app_examfinal_gsa/models/provider_model.dart';
